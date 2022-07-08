1c277ed5ac3478b61ed92acd81de3b61c046b8a1
joshfranzen <38957034+joshfranzen@users.noreply.github.com>
2022-07-08 09:46:14 -0500

    Update main.tf
    Add hub vnet

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
 #Create Resources Below
+#Create Resources Below
+locals {
+    prefix-hub         = "hub"
+    hub-location       = "eastus"
+    hub-resource-group = "rg-vnet"
+}
+
+resource "azurerm_resource_group" "hub-vnet-rg" {
+    name     = local.hub-resource-group
+    location = local.hub-location
+}
+
+resource "azurerm_virtual_network" "hub-vnet" {
+    name                = "${local.prefix-hub}-vnet"
+    location            = azurerm_resource_group.hub-vnet-rg.location
+    resource_group_name = azurerm_resource_group.hub-vnet-rg.name
+    address_space       = ["10.0.0.0/16"]
+
+    tags = var.tags
+}
